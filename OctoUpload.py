36db44838066ea7f2248b4265f3163555fd0f5d6
quillford <adeebshihadeh@Adeebs-MacBook-Pro.local>
2015-01-05 12:03:34 -0800

    Gcode is sent | possible working upload functionality

--- OctoUpload.py
+++ OctoUpload.py
@@ -2,9 +2,9 @@
 #Info: Uploads gcode to OctoPrint server after slicing (All fields required)
 #Depend: GCode
 #Type: postprocess
-#Param: hostIP(string:) IP Address
+#Param: hostIP(string:octopi.local) IP Address
 #Param: hostPort(string:80) Port
-#Param: apiKey(string:) API Key
+#Param: apiKey(string:156A8AE4000940CFB3C51C9DFD812D8A) API Key
 #Param: outputName(string:output) Output Filename
 ##Param: autoPrint(string:no) Print on upload (yes/no)
 
@@ -13,6 +13,7 @@
 #option for autostart print
 #option to not upload during real time slicing
 #option for different variations of gcode (.gcode, .gco, .g)
+#get model name & set as output
 
 version = "0.1"
 
@@ -22,16 +23,19 @@ import urllib
 import urllib2
 import json
 
+myGcode = outputName
+myKey = apiKey
+
 #prints user's input
 print "IP: " + hostIP + ":" + hostPort
 print "API key: " + apiKey
-print "Filename: " + outputName + ".gcode"
+print "Filename: " + myGcode + ".gcode"
 #print autoPrint
 
 #makes a copy of the gcode to send to OctoPrint
 with open(filename, "r") as f:
 
-    file = open(outputName + ".gcode", "w")
+    file = open(myGcode + ".gcode", "w")
 
     gcode = f.readlines()
 
@@ -46,19 +50,24 @@ url = "http://" + hostIP + ":" + hostPort + "/api/files/local"
 print "URL: " + url
 
 http_header = {
-                "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
-                "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryDeC2E3iWbTv1PwMC",
+                #"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+                #"Host" : hostIP,
+                #"Accept" : "application/json, text/javascript,*/*;q=0.01",
+                #"Accept-Encoding" : "gzip, deflate",
+                #"X-Requested-With" : "XMLHttpRequest",
+                #"Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryDeC2E3iWbTv1PwMC",
                 #"Content-Type" : "multipart/form-data",
-                #"Content-Type" : "form-data",
-                "X-Api-Key": apiKey,
+                "Content-Type" : "form-data",
+                "X-Api-Key" : myKey
                 }
 payload = {
-    "Content-Disposition" : "form-data; name='file'; filename='octoupload.gcode'",
-    "Content-Type" : "application/octet-stream"
+    "Content-Disposition" : "form-data; name='file'; filename='output.gcode'",
+    "Content-Type" : "application/octet-stream",
+    'file': open(myGcode + ".gcode", 'rb').readlines()
 }
 
 files = {
-    'file': open(outputName + ".gcode", 'rb'),
+    'file': open(myGcode + ".gcode", 'rb'),
     "filename" : "octoupload" + ".gcode"
 }
 
@@ -66,28 +75,25 @@ files = {
 timeout = 15
 socket.setdefaulttimeout(timeout)
 
-# setup cookie handler
-cookie_jar = cookielib.LWPCookieJar()
-cookie = urllib2.HTTPCookieProcessor(cookie_jar)
-
 # create an urllib2 opener()
-#opener = urllib2.build_opener(proxy, cookie) # with proxy
-opener = urllib2.build_opener(cookie) # we are not going to use proxy now
+opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=1))
 
 # create your HTTP request
-req = urllib2.Request(url, urllib.urlencode(payload), http_header, urllib.urlencode(files))
-
+req = urllib2.Request(url, urllib.urlencode(payload), http_header)
+print req
 print "Uploading..."
 
 # submit your request
-res = opener.open(req)
-html = res.read()
-response = urllib2.urlopen(req)
-
-print "Response: " + response
+#res = opener.open(req)
+feeddata = opener.open(req).read()
+# html = res.read()
+# print html
+# response = urllib2.urlopen(req)
+#
+# print "Response: " + response
 
 # save retrieved HTML to file
-open("tmp.html", "w").write(html)
-print "HTML" + html
+# open("tmp.html", "w").write(html)
+# print "HTML" + html
 
 print "done"
