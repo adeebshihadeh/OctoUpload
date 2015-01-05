5f3ecaa2b52d37895ef26ddc6ab8b19248e97292
quillford <adeebshihadeh@Adeebs-MacBook-Pro.local>
2015-01-04 21:19:15 -0800

    Cleaned up code (not working: 400 Bad Request)

--- OctoUpload.py
+++ OctoUpload.py
@@ -1,10 +1,10 @@
 #Name: OctoUpload
-#Info: Uploads gcode to OctoPrint server after slicing
+#Info: Uploads gcode to OctoPrint server after slicing (All fields required)
 #Depend: GCode
 #Type: postprocess
-#Param: hostIP(string:192.168.140) IP Address
-#Param: octoPort(string:80) Port
-#Param: apiKey(string:156A8AE4000940CFB3C51C9DFD812D8A) API Key
+#Param: hostIP(string:) IP Address
+#Param: hostPort(string:80) Port
+#Param: apiKey(string:) API Key
 #Param: outputName(string:output) Output Filename
 ##Param: autoPrint(string:no) Print on upload (yes/no)
 
@@ -12,11 +12,9 @@
 #connect & upload to octo
 #option for autostart print
 #option to not upload during real time slicing
+#option for different variations of gcode (.gcode, .gco, .g)
 
 version = "0.1"
-import re
-
-#import requests
 
 import cookielib
 import socket
@@ -25,9 +23,9 @@ import urllib2
 import json
 
 #prints user's input
-print hostIP + ":" + octoPort
-#print apiKey
-#print outputName
+print "IP: " + hostIP + ":" + hostPort
+print "API key: " + apiKey
+print "Filename: " + outputName + ".gcode"
 #print autoPrint
 
 #makes a copy of the gcode to send to OctoPrint
@@ -43,37 +41,18 @@ with open(filename, "r") as f:
 
     #print gcode
 
-# url = "http://" + hostIP + "/api/files/local"
-# headers = {"content-type" : "form-data"}
-# #headers = {"content-type" : "multipart/form-data"}
-# X-Api-Key = apiKey
-# payload = "path to gcode"
-# files = {'file': open(outputName + ".gcode", 'rb')}
-#
-# r = requests.post(url, headers=headers, files=files)
-# r = requests.get
-# print r.starus_code
-
 #this is for urlib2
-url = "http://" + "octopi.local" + "/api/files/local"
+url = "http://" + hostIP + ":" + hostPort + "/api/files/local"
 print "URL: " + url
 
 http_header = {
                 "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
-                    #switch to user ip var
-                #"Host" : "octopi.local",
-                #"Origin:" : "http://octopi.local",
-                #"Accept" : "application/json, text/javascript, */*; q=0.01",
-                #"Referer" : "http://prusa.local/",
-                "Accept-Language" : "en-US,en;q=0.8",
-                "Accept-Encoding" : "gzip, deflate",
-                #"Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryDeC2E3iWbTv1PwMC",
+                "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryDeC2E3iWbTv1PwMC",
                 #"Content-Type" : "multipart/form-data",
-                "Content-Type" : "form-data",
-                "X-Api-Key": "156A8AE4000940CFB3C51C9DFD812D8A",
-                "X-Requested-With" : "XMLHttpRequest"
+                #"Content-Type" : "form-data",
+                "X-Api-Key": apiKey,
                 }
-data = {
+payload = {
     "Content-Disposition" : "form-data; name='file'; filename='octoupload.gcode'",
     "Content-Type" : "application/octet-stream"
 }
@@ -96,7 +75,7 @@ cookie = urllib2.HTTPCookieProcessor(cookie_jar)
 opener = urllib2.build_opener(cookie) # we are not going to use proxy now
 
 # create your HTTP request
-req = urllib2.Request(url, urllib.urlencode(data), http_header, urllib.urlencode(files))
+req = urllib2.Request(url, urllib.urlencode(payload), http_header, urllib.urlencode(files))
 
 print "Uploading..."
 
